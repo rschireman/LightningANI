adb5cf6f8053dba35af82923fae47a2e4d4bdde2
Ray Schireman <raymondschireman@gmail.com>
2022-04-07 22:06:00 -0400

    Update test_classifier.py

--- tests/test_classifier.py
+++ tests/test_classifier.py
@@ -7,5 +7,7 @@ def test_lit_classifier():
     seed_everything(1234)
 
     model = NNPLightningModel()
-    data = NNPDataModule(data_dir="BTBT-NPT-300K-and-100K.h5'")
+    data = NNPDataModule(data_dir="BTBT-NPT-300K-and-100K.h5'",batch_size=32)
+    trainer = Trainer(max_epochs=2)
+    trainer.fit(model, data)
    
