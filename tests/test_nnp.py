bb5fbf28aa5e3fbcb5f9a9b59a81a9b1c3308c2e
Ray Schireman <raymondschireman@gmail.com>
2022-04-07 22:54:46 -0400

    Update test_nnp.py

--- tests/test_nnp.py
+++ tests/test_nnp.py
@@ -3,14 +3,13 @@ from NNP.nnp import NNPLightningModel
 from NNP.nnp import NNPDataModule
 
 
-def test_lit_classifier():
-    seed_everything(1234)
 
-    data = NNPDataModule(data_dir="./BTBT-NPT-300K-and-100K.h5'",batch_size=32)
+def test_nnp():
+    seed_everything(1234)
+    data = NNPDataModule(data_dir="./BTBT-NPT-300K-and-100K.h5'", batch_size=32)
     aev_dim = data.get_aev_dim()
     aev_computer = data.aev_computer
     model = NNPLightningModel(aev_computer=aev_computer, aev_dim=aev_dim,learning_rate=1e-5)
-    print(data)
     trainer = Trainer(max_epochs=2)
-    trainer.fit(model, data)
+    assert data is not None and model is not None and trainer is not None
    
