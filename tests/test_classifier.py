7010c3a5ddafdea254866fcbc1c036ff420f238c
Ray Schireman <raymondschireman@gmail.com>
2022-04-07 20:53:24 -0400

    add test dataset

--- tests/test_classifier.py
+++ tests/test_classifier.py
@@ -7,5 +7,5 @@ def test_lit_classifier():
     seed_everything(1234)
 
     model = NNPLightningModel()
-    data = NNPDataModule(data_dir="'C:\\Users\\ray\\Dropbox\\ML\\NNPs\\BPNNs\\torchANI\\100000_steps_100K\\NNP\\BTBT-NPT-300K-and-100K.h5'")
+    data = NNPDataModule(data_dir="BTBT-NPT-300K-and-100K.h5'")
    
