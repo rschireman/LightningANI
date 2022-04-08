5f8aacc886e86c8d7cafb2c5d28c085ff3f164b8
Ray Schireman <raymondschireman@gmail.com>
2022-04-07 20:45:48 -0400

    fix ci testing

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='project',
+    name='NNP',
     version='0.0.0',
     description='Describe Your Cool Project',
-    author='',
+    author='Ray Schireman',
     author_email='',
     # REPLACE WITH YOUR OWN GITHUB PROJECT LINK
     url='https://github.com/rschireman/ANI-NNP-pl',
