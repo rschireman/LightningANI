33c8e5adc55c6cac22c9d7810d39ab61eef374c3
Ray Schireman <raymondschireman@gmail.com>
2022-04-07 22:18:51 -0400

    update to python 3.8

--- tests/test_classifier.py
+++ tests/test_classifier.py
@@ -6,7 +6,7 @@ from NNP.nnp import NNPDataModule
 def test_lit_classifier():
     seed_everything(1234)
 
-    data = NNPDataModule(data_dir="BTBT-NPT-300K-and-100K.h5'",batch_size=32)
+    data = NNPDataModule(data_dir="./BTBT-NPT-300K-and-100K.h5'",batch_size=32)
     aev_dim = data.get_aev_dim()
     aev_computer = data.aev_computer
     model = NNPLightningModel(aev_computer=aev_computer, aev_dim=aev_dim)
