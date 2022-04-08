27ce236c2c4ae151ceb84ae50996d8a6e90ac3e6
Raymond G Schireman <raymond.schireman@uvm.edu>
2022-04-08 11:21:47 -0400

    log lr and write pt every epoch

--- NNP/nnp_sigmoid.py
+++ NNP/nnp_sigmoid.py
@@ -91,7 +91,6 @@ class NNPLightningSigmoidModel(pl.LightningModule):
           
         def configure_optimizers(self):
             optimizer = torch.optim.Adam(self.parameters(), lr=1e-5)
-           
             return optimizer
         
         def forward(self, species, coordinates):
