ed367cc21643e3706f90fd661dfad386e61dcb39
Ray Schireman <raymondschireman@gmail.com>
2022-04-07 22:11:36 -0400

    Update test_classifier.py

--- tests/test_classifier.py
+++ tests/test_classifier.py
@@ -6,8 +6,10 @@ from NNP.nnp import NNPDataModule
 def test_lit_classifier():
     seed_everything(1234)
 
-    model = NNPLightningModel()
     data = NNPDataModule(data_dir="BTBT-NPT-300K-and-100K.h5'",batch_size=32)
-    trainer = Trainer(max_epochs=2)
+    aev_dim = data.get_aev_dim()
+    aev_computer = data.aev_computer
+    model = NNPLightningModel()
+    trainer = Trainer(max_epochs=2, aev_computer=aev_computer, aev_dim=aev_dim)
     trainer.fit(model, data)
    
