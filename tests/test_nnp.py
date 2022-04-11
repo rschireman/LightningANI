14670add140cf29c8bcdfbf8c8e6976adfa326d1
Raymond G Schireman <raymond.schireman@uvm.edu>
2022-04-11 13:24:46 -0400

    allow for force training to start after #epochs input

--- tests/test_nnp.py
+++ tests/test_nnp.py
@@ -2,7 +2,7 @@ from pytorch_lightning import Trainer, seed_everything
 from NNP.nnp import NNPLightningModel
 from NNP.nnp_data_module import NNPDataModule
 from NNP.nnp_sigmoid import NNPLightningSigmoidModel
-
+from NNP.nnp_delayed_force_training import NNPLightningSigmoidModelDF
 
 def test_nnp():
     seed_everything(1234)
@@ -19,5 +19,14 @@ def test_nnp_sigmoid():
     aev_dim = data.get_aev_dim()
     aev_computer = data.aev_computer
     model = NNPLightningSigmoidModel(aev_computer=aev_computer, aev_dim=aev_dim,learning_rate=1e-6)
+    trainer = Trainer(max_epochs=1)
+    assert data is not None and model is not None and trainer is not None
+
+def test_nnp_sigmoid_df():
+    seed_everything(1234)
+    data = NNPDataModule(data_dir="BTBT-NPT-300K-and-100K.h5", batch_size=32)
+    aev_dim = data.get_aev_dim()
+    aev_computer = data.aev_computer
+    model = NNPLightningSigmoidModelDF(aev_computer=aev_computer, aev_dim=aev_dim,learning_rate=1e-6)
     trainer = Trainer(max_epochs=1)
     assert data is not None and model is not None and trainer is not None
