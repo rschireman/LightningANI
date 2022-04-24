8ba0b97e75a1087e9463e0e1c062d2385d439b46
Ray Schireman <raymondschireman@gmail.com>
2022-04-23 22:45:28 -0400

    add EtaR input

--- lightning_ani/nnp_data_module.py
+++ lightning_ani/nnp_data_module.py
@@ -7,13 +7,13 @@ import torchani.data
 
 
 class NNPDataModule(pl.LightningDataModule):
-    def __init__(self, data_dir: str = "./", batch_size: int=32, use_cuda_extension: bool = False, Rcr: float=5.200, Rca: float=3.50):
+    def __init__(self, data_dir: str = "./", batch_size: int=32, use_cuda_extension: bool = False, Rcr: float=5.200, Rca: float=3.50, EtaR: torch.tensor=[16.00]):
         super().__init__()
         
 
         self.Rcr = Rcr
         self.Rca = Rca
-        self.EtaR = torch.tensor([1.6000000e+01])
+        self.EtaR = EtaR
         self.ShfR = torch.tensor([9.0000000e-01, 1.1687500e+00, 1.4375000e+00, 1.7062500e+00, 1.9750000e+00, 2.2437500e+00, 2.5125000e+00, 2.7812500e+00, 3.0500000e+00, 3.3187500e+00, 3.5875000e+00, 3.8562500e+00, 4.1250000e+00, 4.3937500e+00, 4.6625000e+00, 4.9312500e+00])
         self.Zeta = torch.tensor([3.2000000e+01])
         self.ShfZ = torch.tensor([1.9634954e-01, 5.8904862e-01, 9.8174770e-01, 1.3744468e+00, 1.7671459e+00, 2.1598449e+00, 2.5525440e+00, 2.9452431e+00])
