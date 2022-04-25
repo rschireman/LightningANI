6796e253421d8ac2c808f982636434484bd573a2
Raymond G Schireman <raymond.schireman@uvm.edu>
2022-04-25 09:41:09 -0400

    inc early stopping patience

--- lightning_ani/nnp_delayed_force_training.py
+++ lightning_ani/nnp_delayed_force_training.py
@@ -158,7 +158,7 @@ def cli_main():
     # training
     # ------------
     checkpoint_callback = ModelCheckpoint(dirpath="runs", save_top_k=20, monitor="val_force_loss")
-    early_stopping = EarlyStopping(monitor="val_force_loss", mode="min", patience=10)
+    early_stopping = EarlyStopping(monitor="val_force_loss", mode="min", patience=75)
     trainer = pl.Trainer.from_argparse_args(args, gpus=1, max_epochs=1000, callbacks=[checkpoint_callback, early_stopping],auto_lr_find=True,auto_scale_batch_size='binsearch')
     trainer.tune(nnp, data)
     trainer.fit(nnp, data)
