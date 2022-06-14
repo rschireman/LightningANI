8faa50befe865c9a04472b4fac9bfcfd9f3b0a1e
Raymond G Schireman <raymond.schireman@uvm.edu>
2022-06-14 13:52:29 -0400

    clean main folder

--- tests/test_nnp.py
+++ tests/test_nnp.py
@@ -2,7 +2,7 @@ from pytorch_lightning import Trainer, seed_everything
 from lightning_ani.nnp import NNPLightningModel
 from lightning_ani.nnp_data_module import NNPDataModule
 from lightning_ani.nnp_sigmoid import NNPLightningSigmoidModel
-from lightning_ani.nnp_delayed_force_training import NNPLightningModelDF
+from lightning_ani.nnp import NNPLightningModelDF
 
 def test_nnp():
     seed_everything(1234)
@@ -13,20 +13,3 @@ def test_nnp():
     trainer = Trainer(max_epochs=1)
     assert data is not None and model is not None and trainer is not None
 
-def test_nnp_sigmoid():
-    seed_everything(1234)
-    data = NNPDataModule(data_dir="BTBT-NPT-300K-and-100K.h5", batch_size=32)
-    aev_dim = data.get_aev_dim()
-    aev_computer = data.aev_computer
-    model = NNPLightningSigmoidModel(aev_computer=aev_computer, aev_dim=aev_dim,learning_rate=1e-6)
-    trainer = Trainer(max_epochs=1)
-    assert data is not None and model is not None and trainer is not None
-
-def test_nnp_sigmoid_df():
-    seed_everything(1234)
-    data = NNPDataModule(data_dir="BTBT-NPT-300K-and-100K.h5", batch_size=32)
-    aev_dim = data.get_aev_dim()
-    aev_computer = data.aev_computer
-    model = NNPLightningModelDF(aev_computer=aev_computer, aev_dim=aev_dim,learning_rate=1e-6)
-    trainer = Trainer(max_epochs=1)
-    assert data is not None and model is not None and trainer is not None
